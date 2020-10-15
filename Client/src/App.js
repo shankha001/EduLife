4a52c900ca4d5da10fed4c3b3855e09dd3092f62
shankha001 <shankhasubhrabiswas@gmail.com>
2020-10-15 23:41:52 +0530

    Add react-router-dom and config..

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
-import "./App.css";
-import Navbar from "./Components/Navbar";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
