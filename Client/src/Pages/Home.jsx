d4ba6414ca55327271c6fcf14e322334ccd78faf
shankha001 <shankhasubhrabiswas@gmail.com>
2020-10-18 20:54:31 +0530

    update homepage

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Lottie from 'react-lottie';
+import Classes from '../Assets/class.json';
 
 function Home() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: Classes,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div style={{ height: '90vh' }}>
+      <h1 style={{ textAlign: 'center', textTransform: 'uppercase' }}>
+        Edulife - Hackccelarate 2020 🚀
+      </h1>
+      <Lottie options={defaultOptions} height={500} width={800} />
     </div>
   );
 }
